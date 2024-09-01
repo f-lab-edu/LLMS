761758dedd26b7d63cec59cdd703620031b68890
sik2 <bickwang123@gmail.com>
2024-09-01 18:57:50 +0900

    kopring init

---
+++ src/main/kotlin/lab/loop/llmskt/LlmsKtApplication.kt
@@ -0,0 +1,11 @@
+package lab.loop.llmskt
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class LlmsKtApplication
+
+fun main(args: Array<String>) {
+    runApplication<LlmsKtApplication>(*args)
+}
